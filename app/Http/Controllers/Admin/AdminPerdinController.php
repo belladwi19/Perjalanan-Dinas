ac203c80e1b785a2413734e5308bd75ea062da46
belladwi19 <bellamardiana19@gmail.com>
2023-10-26 09:17:58 +0700

    commit 1-authentication (register only view)

---
+++ app/Http/Controllers/Admin/AdminPerdinController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Perjalanan;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Response;
+
+class AdminPerdinController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $perjalanan = Perjalanan::with(['user'])->paginate(7);
+
+        return view('Admin.DataPerdin.DataPerdin', compact('perjalanan'));
+    }
+
+    public function detail($id)
+    {
+        $perjalanan = Perjalanan::with(['user'])->findOrFail($id);
+
+        return view('Admin.DataPerdin.DetailPerdin', compact('perjalanan') );
+    }
+}
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Aplikasi Perjalanan Dinas</title>
+  
+  <link rel="stylesheet" href="../css/styles.min.css" />
+</head>
+
+<body>
+  <!--  Body Wrapper -->
+  <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+    data-sidebar-position="fixed" data-header-position="fixed">
+    <div
+      class="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
+      <div class="d-flex align-items-center justify-content-center w-100">
+        <div class="row justify-content-center w-100">
+          <div class="col-md-8 col-lg-6 col-xxl-3">
+            <div class="card mb-0">
+              <div class="card-body">
+                <a href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
+                  <img src="../images/logos/logo.webp" width="180" alt="">
+                </a>
+                <form>
+                @csrf
+                  <div class="mb-3">
+                    <label for="exampleInputtext1" class="form-label">Name</label>
+                    <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}"  autofocus>
+
+                              
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email Address</label>
+                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" >
+
+                                
+                  </div>
+                  <div class="mb-4">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input id="password" type="password" class="form-control" name="password">
+
+                                
+                  </div>
+                  <div class="mb-4">
+                    <label for="exampleConfirmInputPassword1" class="form-label">Confirm Password</label>
+                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation">
+                  </div>
+                  <button type="submit" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Sign Up</button>
+                  <div class="d-flex align-items-center justify-content-center">
+                    <p class="fs-4 mb-0 fw-bold">Already have an Account?</p>
+                    @if (Route::has('login'))
+                        <a class="text-primary fw-bold ms-2" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    @endif
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="../libs/jquery/dist/jquery.min.js"></script>
+  <script src="../libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
